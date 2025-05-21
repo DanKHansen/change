188a82c0d5b621d97f7132cfecd51cbe77c9a19d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-21 11:17:01 +0200

    refactor

--- src/main/scala/Change.scala
+++ src/main/scala/Change.scala
@@ -1,10 +1,10 @@
 object Change:
-   def findFewestCoins(amount: Int, coins: List[Int]): Option[List[Int]] =
-      if amount > 0 then
-         val dp = (0 to amount).foldLeft(Vector.fill(amount + 1)(None: Option[List[Int]])) { (acc, amt) =>
+   def findFewestCoins(target: Int, coins: List[Int]): Option[List[Int]] =
+      if target >= 0 then
+         val dp = (0 to target).foldLeft(Vector.fill(target + 1)(None: Option[List[Int]])) { (acc, amt) =>
             if amt == 0 then acc.updated(amt, Some(List.empty[Int])) else
                val candidates = coins.flatMap { coin => if amt - coin >= 0 then acc(amt - coin).map(coin :: _) else None}
                acc.updated(amt, if candidates.isEmpty then None else Some(candidates.minBy(_.length)))
          }
-         dp(amount)
+         dp(target)
       else None
